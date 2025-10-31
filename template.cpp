6af90f2b1650e4d81002848c115aa956fcdebdf1
MARCELO LEMUS <marcelolemus10@gmail.com>
2025-10-30 21:05:17 -0300

    feat(template): added template

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+#define rep(i, n) for(int i = 0; i < n; i++)
+#define repx(i, a, b) for(int i = a; i<(int)b; ++i)
+#define vl vector<ll>
+#define vi vector<int>
+
+using ll = long long;
+using pii = pair<int, int>;
+
+// mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve(){;}
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int T = 1;
+    //cin >> T;
+    while(T--){
+        solve();
+    }
+}
