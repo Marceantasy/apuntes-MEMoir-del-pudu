0833cee00d0fc44cf8602a8f6f4e68e6c3f3438c
Enzo Vivallo <enzovivallo05@outlook.com>
2026-01-09 13:18:28 -0300

    fix(geometry): fix abs

--- geometry/point-in-triangle.cpp
+++ geometry/point-in-triangle.cpp
@@ -1,11 +1,11 @@
 // Check if point is in Triangle abc
 // IMPORTANT: Points on the edge are considered inside
 // IMPORTANT: Check for overflow (int128 may be needed)
+// int128 needs custom abs function
 bool pointInTriangle(Point a, Point b, Point c, Point point) {
-  auto area = absolute(a.cross(b, c));
-  auto candidateArea = absolute(point.cross(a, b)) +
-                       absolute(point.cross(b, c)) +
-                       absolute(point.cross(c, a));
+  auto area = abs(a.cross(b, c));
+  auto candidateArea =
+      abs(point.cross(a, b)) + abs(point.cross(b, c)) + abs(point.cross(c, a));
 
   return area == candidateArea;
 }
