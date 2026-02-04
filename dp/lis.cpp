00ea419e63a58e3ba289e502285f8e292a04260f
Enzo Vivallo <enzovivallo05@outlook.com>
2026-02-04 19:00:49 -0300

    feat(dp): add lis

---
+++ dp/lis.cpp
@@ -0,0 +1,14 @@
+int find_lis(const vector<int> &a) {
+  vector<int> dp;
+
+  for (int i : a) {
+    int pos = lower_bound(dp.begin(), dp.end(), i) - dp.begin();
+
+    if (pos == dp.size())
+      dp.push_back(i);
+    else
+      dp[pos] = i;
+  }
+
+  return dp.size();
+}
