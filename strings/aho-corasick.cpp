8ba2807e5b57ad2c975a6962184394cdcb0e5fcf
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2026-02-22 20:22:23 -0300

    fix(strings/aho-corasick.cpp): array N now considers the failure link
    Refactor Aho-Corasick structure for clarity and efficiency.

--- strings/aho-corasick.cpp
+++ strings/aho-corasick.cpp
@@ -1,53 +1,47 @@
-struct AC
-{
-    ll c = 0, ec = 0, M, A, af = -1;
-    vector<vl> N, G; vl L, E;
-    vector<vl> val;
-    vl vis, cnt;
-    // L -> suffix link G -> anti L
-    // E -> string finish
-    AC (ll M, ll A) : M(M), A(A), N(M, vl(A, 0)), G(M, vl(0)), E(M, 0), L(M, 0), val(M, vl(0)), vis(M, 0), cnt(M, 0){}
-    ll add(string s, int id){ // return endpoint
-        af++;
+struct AC{ // code for finding number of ocurrences of each pattern
+    ll c = 0, M, A;
+    vector<vl> N, G; vl L, cnt;
+    vector<vi> val;
+    // L -> suffix link (G -> anti L (u -> v if u is suffix of v))
+    AC (ll M, ll A) : M(M), A(A), N(M, vl(A, 0)), G(M, vl()), L(M, 0), val(M, vi()), cnt(M, 0){}
+    
+    ll add(string &s, int id){ // return endpoint
         ll p = 0;
         for (char l : s){
             int t = l - 'a';
-            if (!N[p][t]) N[p][t] = ++c;
+            if(!N[p][t]) N[p][t] = ++c;
             p = N[p][t];
         }
         val[p].push_back(id);
         return p;
     }
-    // construct aho corasick structure
     void init(){
-        queue<int> q; q.push(0); L[0] = -1;
-        while (!q.empty()){
-            int p = q.front(); q.pop();
-            for(int c = 0; c < A; c++){
-                int u = N[p][c]; if (!u) continue;
-                L[u] = L[p] == -1 ? 0 : N[L[p]][c], q.push(u);
-                G[L[u]].push_back(u);
+        queue<int> q;
+        for(int i = 0; i < A; i++) if(N[0][i]) q.push(N[0][i]);
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            for(int i = 0; i < A; i++){
+                int v = N[u][i];
+                if(v) { L[v] = N[L[u]][i]; q.push(v); }
+                else N[u][i] = N[L[u]][i];
             }
-            if (p) for(int c = 0; c < A; c++) if (!N[p][c]) N[p][c] = N[L[p]][c];
+            G[L[u]].push_back(u); // Build fail tree
         }
     }
-    // para un texto s, recorrer las aristas por el arbol
     void run(string &s){
-        for(int p = 0, i = 0; i<(int)s.size(); ++i){
-            int t = s[i]-'a';
-            p = (N[p][t] == -1 ? 0 : N[p][t]);
+        int p = 0;
+        for(char l : s){
+            p = N[p][l-'a'];
             cnt[p]++;
         }
     }
-    void dfs(int u = 0){
-        vis[u] = true;
+    void pull(int u = 0){
         for(int v : G[u]){
-            if(v == -1) continue;
-            if(!vis[v]) dfs(v);
+            pull(v);
             cnt[u] += cnt[v];
         }
-        for(int v : val[u]){
-            ans[v] = cnt[u];
+        for(int id : val[u]){
+            ans[id] = cnt[u];
         }
     }
 };
