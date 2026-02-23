25bdd8e06303b3eba2514c287f01075f485ea089
MarceloL <55332694+Marceantasy@users.noreply.github.com>
2026-02-22 22:15:51 -0300

    feat: add phi euler function for mobius problems

--- math/mobius.cpp
+++ math/mobius.cpp
@@ -2,3 +2,14 @@ short mu[MAXN] = {0,1};
 void mobius(){
     repx(i,1,MAXN)if(mu[i])for(int j=i+i;j<MAXN;j+=i)mu[j]-=mu[i];
 }
+
+void phi_1_to_n(int n) {
+    vector<int> phi(n+1);
+    for(int i = 0; i<=n; i++) phi[i] = i;
+    for(int i = 2; i<=n; i++){
+        if(phi[i] == i){
+            for (int j = i; j<=n; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
